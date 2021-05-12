87d1bc888f7795b2fa921e9b539e5639de711f10
Youngwok Kim <youngwok.kim83@gmail.com>
2021-05-12 06:22:30 -0400

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p7.jpg',
+            price:10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id:'2',
+            name:'Nike Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p2.jpg',
+            price:100,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id:'3',
+            name:'Lacoste Free Shirt',
+            category: 'Shirts',
+            image:'/images/p8.jpg',
+            price:220,
+            brand: 'Lacoste',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id:'4',
+            name:'Adidas Slim Pants',
+            category: 'Shirts',
+            image:'/images/p4.jpg',
+            price:1120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id:'5',
+            name:'Puma Slim Pants',
+            category: 'Shirts',
+            image:'/images/p5.jpg',
+            price:160,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id:'6',
+            name:'Adidas Fit Pants',
+            category: 'Shirts',
+            image:'/images/p9.jpg',
+            price:110,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        }
+    ]
+}
+
+export default data;
